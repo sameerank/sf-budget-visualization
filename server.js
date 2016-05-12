e814312e694e4294f04a65fbbf99eaf88cd9d985
sameerank <skunche@gmail.com>
2016-05-12 14:38:48 -0700

    switching to express

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-var server = require('pushstate-server');
+var express = require('express');
+var app = express();
 
-server.start({
-  port: 5000,
-  directories: [
-    './'
-  ]
-}).listen(process.env.PORT || 5000);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
